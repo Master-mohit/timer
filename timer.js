48308216ce6b08919d7b817a8853df90dfdf4168
Master-mohit <mohitmansigupta9098@gmail.com>
2025-05-17 13:50:44 +0530

    timer function

---
+++ timer.js
@@ -0,0 +1,24 @@
+//  let count = 0;
+//   const timer = document.getElementById('progressText');
+
+//   const interval = setInterval(() => {
+//     count++;
+//     timer.innerText = "Progress: " + count + "%";
+//     if (count === 100) {
+//       clearInterval(interval);
+//     }
+//   }, 100);
+
+  let count = 0;
+const timer = document.getElementById('progressText');
+
+const interval = setInterval(() => {
+
+  let randomIncrement = Math.floor(Math.random() * 10) + 1; 
+  count += randomIncrement;
+  if(count > 100) count = 100; 
+  timer.innerText = "Progress: " + count + "%";
+  if (count === 100) {
+    clearInterval(interval);
+  }
+}, 100);
